a1db56bdb79da341d27050b09e6c6c7d19d67391
Kris1027 <obarzanek.work@gmail.com>
2024-10-17 10:50:32 +0200

    fizbuzz game

---
+++ 5-fizz-buzz-game.cs
@@ -0,0 +1,32 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool threeDiv = false;
+        bool fiveDiv = false;
+        
+        for (int i = 1; i <= 100; i++)
+        {
+
+            threeDiv = i % 3 == 0;
+            fiveDiv = i % 5 == 0;
+            
+            if (threeDiv && fiveDiv)
+            {
+                Console.WriteLine("FizzBuzz");
+            } 
+            else if (threeDiv)
+            {
+                Console.WriteLine("Fizz");
+            } 
+            else if (fiveDiv)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
